3e8ae0af5c5fc0b859fb25037cddddd2feefd02f
Tamal Sur <tamalsur14@gmail.com>
2024-07-07 18:07:45 +0530

    completed

--- src/components/WelcomeMessage.jsx
+++ src/components/WelcomeMessage.jsx
@@ -4,9 +4,7 @@ const WelcomeMessage = () => {
   return (
     <center className='welcome_msg '>
         <h1 className='display-3'>There are no posts Yet</h1>
-        <button>
-         type="button"
-        </button>
+
          
     </center>
   )
